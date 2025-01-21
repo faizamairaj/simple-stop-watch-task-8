a97dcb30564c80ce544c0b77dd4c436ff9a99b93
faizamairaj <faizamairaj0@gmail.com>
2025-01-21 12:55:24 +0500

    simple-stop-watch task-8

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,19 @@ function App() {
   };
 
   return (
-    <div>
-      <i><h1 className='text-8xl font-bold text-red-500'>Stopwatch</h1></i>
+    <div
+      className="flex flex-col items-center justify-center h-screen"
+      style={{ backgroundImage: "url('image.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
+      <i><h1 className='text-5xl font-bold text-red-500'>Stopwatch</h1></i>
       <i><h2 className='font-bold text-6xl bg-red-500 text-white p-4 mt-10 rounded-lg border border-yellow-500 shadow-2xl'>{time} times</h2></i>
       <div className="flex justify-center space-x-10 mt-5">
-  <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleStart}>Start</button>
-  <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleStop}>Stop</button>
-  <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleReset}>Reset</button>
-</div>
+        <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleStart}>Start</button>
+        <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleStop}>Stop</button>
+        <button className='bg-blue-600 text-white text-2xl font-bold p-3 rounded-md' onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
+
 export default App;
